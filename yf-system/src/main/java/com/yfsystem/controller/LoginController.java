f5c12aac628ee60f25fad2338c9a6a79b69381b4
“ouyangyifan” <yf020716@163.com>
2024-10-22 20:37:01 +0800

    新建工程

---
+++ yf-system/src/main/java/com/yfsystem/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.yfsystem.controller;
+
+/**
+ * ClassName LoginController
+ * Author OuYangYiFan
+ * Description TODO
+ * DATA 2024/10/22 下午5:32
+ * Version 1.0
+ */
+public class LoginController {
+
+}
